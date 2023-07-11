99cb0fdc754e8eec65ebe8c0a92bdebee3eeed77
mcflinta <21521901@gm.uit.edu.vn>
2023-07-11 16:25:56 +0700

    add lab 6

---
+++ NT106.N22.MMCL.1-Lab06-21521901-21522659-21521865/Lab6/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Lab6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbIpAddr = new System.Windows.Forms.TextBox();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.dgvRemoteControls = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRemoteControls)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP FTP Server";
+            // 
+            // tbIpAddr
+            // 
+            this.tbIpAddr.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbIpAddr.Location = new System.Drawing.Point(133, 31);
+            this.tbIpAddr.Margin = new System.Windows.Forms.Padding(4);
+            this.tbIpAddr.Name = "tbIpAddr";
+            this.tbIpAddr.Size = new System.Drawing.Size(421, 25);
+            this.tbIpAddr.TabIndex = 1;
+            this.tbIpAddr.Text = "ftp://127.0.0.1/";
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUsername.Location = new System.Drawing.Point(133, 64);
+            this.tbUsername.Margin = new System.Windows.Forms.Padding(4);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(421, 25);
+            this.tbUsername.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 67);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Username";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPassword.Location = new System.Drawing.Point(133, 97);
+            this.tbPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(421, 25);
+            this.tbPassword.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 100);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(35, 141);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(139, 43);
+            this.btnConnect.TabIndex = 6;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 206);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Remote Site";
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(575, 256);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(139, 43);
+            this.btnUpload.TabIndex = 9;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(575, 322);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(139, 43);
+            this.btnDownload.TabIndex = 10;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // dgvRemoteControls
+            // 
+            this.dgvRemoteControls.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvRemoteControls.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRemoteControls.Location = new System.Drawing.Point(35, 239);
+            this.dgvRemoteControls.Name = "dgvRemoteControls";
+            this.dgvRemoteControls.Size = new System.Drawing.Size(519, 150);
+            this.dgvRemoteControls.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 417);
+            this.Controls.Add(this.dgvRemoteControls);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbIpAddr);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRemoteControls)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbIpAddr;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.DataGridView dgvRemoteControls;
+    }
+}
+
---
+++ NT106.N22.MMCL.1-Lab06-21521901-21522659-21521865/Lab6/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string host { get; set; }
+        private string user { get; set; }
+        private string password { get; set; }
+        public string pathFile { get; set; }
+        private FtpWebRequest ftpRequest { get; set; }
+        private FtpWebResponse ftpResponse { get; set; }
+        private Stream ftpStream  { get; set; }
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("Tên tập tin", typeof(string));
+            try
+            {
+                user = tbUsername.Text;
+                password = tbPassword.Text;
+                host = tbIpAddr.Text;
+                ftpRequest = (FtpWebRequest)FtpWebRequest.Create(host + '/' + "shared/");
+                ftpRequest.Credentials = new NetworkCredential(user, password);
+                ftpRequest.UseBinary = true;
+                ftpRequest.UsePassive = true;
+                ftpRequest.KeepAlive = true;
+                ftpRequest.Method = WebRequestMethods.Ftp.ListDirectory;
+                ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
+                ftpStream = ftpResponse.GetResponseStream();
+                StreamReader reader = new StreamReader(ftpStream);
+                string line = reader.ReadLine();
+                while (!string.IsNullOrEmpty(line))
+                {
+                    string fileName = line;
+                    dataTable.Rows.Add(fileName);
+                    line = reader.ReadLine();
+                }
+                dgvRemoteControls.DataSource = dataTable;
+                reader.Close();
+            }
+            catch(WebException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                ftpStream.Close();
+                ftpResponse.Close();
+            }
+        }
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            DataTable dataTable = new DataTable();
+
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.ShowDialog();
+                byte[] fileData = File.ReadAllBytes(ofd.FileName);
+                ftpRequest = (FtpWebRequest)FtpWebRequest.Create(host + '/' + "shared/" + ofd.SafeFileName);
+                ftpRequest.Credentials = new NetworkCredential(user, password);
+                ftpRequest.Method = WebRequestMethods.Ftp.UploadFile;
+                ftpRequest.UseBinary = true;
+                using (Stream requestStream = ftpRequest.GetRequestStream())
+                {
+                    requestStream.Write(fileData, 0, fileData.Length);
+                }
+                ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
+                MessageBox.Show("Tải lên thành công");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                ftpStream.Close();
+                ftpResponse.Close();
+            }
+        }
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string fileName = dgvRemoteControls.CurrentCell.Value.ToString();
+                SaveFileDialog ofd = new SaveFileDialog();
+                ofd.ShowDialog();
+                ftpRequest = (FtpWebRequest)FtpWebRequest.Create(host + '/' + "shared/" + fileName);
+                ftpRequest.Credentials = new NetworkCredential(user, password);
+                ftpRequest.Method = WebRequestMethods.Ftp.DownloadFile;
+                ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
+                using (Stream responseStream = ftpResponse.GetResponseStream())
+                {
+                    using (FileStream fileStream = new FileStream(ofd.FileName, FileMode.Create))
+                    {
+                        byte[] buffer = new byte[1024];
+                        int bytesRead;
+                        while ((bytesRead = responseStream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            fileStream.Write(buffer, 0, bytesRead);
+                        }
+                    }
+                }
+                MessageBox.Show("Tải file thành công");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message);
+            }
+            finally
+            {
+                ftpResponse.Close();
+            }
+
+        }
+    }
+}
